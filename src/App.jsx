d76d04ad39db6c4bbbddcde40e21691a79685aae
NazSvn <idler90@gmail.com>
2024-10-27 12:41:14 +0800

    Added more components to the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,40 @@ import Accordion from './Accordion/Accordian';
 import './App.css';
 import RandomColor from './ColorGenerator/RandomColor';
 import ImageSlider from './ImageSlider/ImageSlider';
+import LoadMore from './LoadMore/LoadMore';
+import NavigationMenu from './NavigationMenu/NavigationMenu';
 import StarRating from './StarRating/StarRating';
+
+ 
 
 function App() {
   return (
     <>
+      {/* accorrdion element */}
+
       <Accordion />
+
+      {/* Star Rating element */}
+
       <StarRating />
+
+      {/* Color generator element */}
+
       <RandomColor />
+
+      {/* Image slider element */}
+
       <ImageSlider
-        url={'https://picsum.photos/v2/list'}
-        limit={'10'}
-        page={'2'}
+        url={'https://picsum.photos/v2/list'}         
       />
+
+      {/* Load more element */}
+
+      <LoadMore />
+
+      {/* Navigation Menu */}
+
+      <NavigationMenu />
     </>
   );
 }
