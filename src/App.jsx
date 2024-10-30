b708c6c89c21d39af40fc1f9a586ba71545c255a
NazSvn <idler90@gmail.com>
2024-10-30 18:56:34 +0800

    Added 3 new components to the project.
    -Added LightDarkTheme component.
    -Added ScrollProgress component.
    -Added TabsTest component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,47 @@
 import Accordion from './Accordion/Accordian';
 import './App.css';
 import RandomColor from './ColorGenerator/RandomColor';
+import TabsTest from './CustomTabs/TabsTest';
 import ImageSlider from './ImageSlider/ImageSlider';
+import LightDarkTheme from './LightDarkTheme/LightDarkTheme';
 import LoadMore from './LoadMore/LoadMore';
 import NavigationMenu from './NavigationMenu/NavigationMenu';
 import QrCodeGenerator from './QrCodeGenerator/QrCodeGenerator';
+import ScrollProgress from './ScrollProgress/ScrollProgress';
 import StarRating from './StarRating/StarRating';
 
 function App() {
   return (
     <>
       {/* accorrdion element */}
-      {/* <Accordion /> */}
+      <Accordion />
 
       {/* Star Rating element */}
-      {/* <StarRating /> */}
+      <StarRating />
 
       {/* Color generator element */}
-      {/* <RandomColor /> */}
+      <RandomColor />
 
       {/* Image slider element */}
-      {/* <ImageSlider
-        url={'https://picsum.photos/v2/list'}         
-      /> */}
+      <ImageSlider url={'https://picsum.photos/v2/list'} />
 
       {/* Load more element */}
-      {/* <LoadMore /> */}
+      <LoadMore />
 
       {/* Navigation Menu */}
-      {/* <NavigationMenu /> */}
+      <NavigationMenu />
 
       {/* Qr code generator */}
       <QrCodeGenerator />
+
+      {/* Light and dark theme */}
+      <LightDarkTheme />
+
+      {/* Scroll progress bar */}
+      <ScrollProgress />
+
+      {/* Tabs  */}
+      <TabsTest />
     </>
   );
 }
