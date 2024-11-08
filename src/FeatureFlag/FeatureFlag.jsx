03b4279ef8644939ef4a4c29d5496cec029f101e
NazSvn <idler90@gmail.com>
2024-11-09 00:05:09 +0800

    feat: Integrate feature flags into FeatureFlag component
    - Leveraged the useFeatureFlag hook to check the enabled state of each feature flag
    - Applied the feature flag logic to conditionally render different UI elements
    - Added a useEffect hook to update the theme based on the dark mode flag
    - Displayed appropriate loading and error states when fetching the feature flags

--- src/FeatureFlag/FeatureFlag.jsx
+++ src/FeatureFlag/FeatureFlag.jsx
@@ -1,22 +1,66 @@
-import { useContext } from 'react';
-import { FeatureFlagsContext } from './contextFlag/ContextFlag';
+import { useContext, useEffect } from 'react';
+import { FeatureFlagsContext, useFeatureFlag } from './contextFlag/ContextFlag';
+import useThemeToggle from '../LightDarkTheme/useThemeToggle';
+import { FLAGS } from './flagsData';
 
-const FeatureFlag = () => {
-  const { loading, error, flags } = useContext(FeatureFlagsContext);
-  console.log(loading, error, flags);
+const NewHeader = () => {
   return (
     <>
-      <div>
-        <div>
-          <button>
-            <header></header>
-            <main></main>
-            {/* theme */}
-            {/* content */}
-          </button>
-        </div>
-      </div>
+      <div>New Header</div>
     </>
+  );
+};
+
+const FeatureFlag = () => {
+  const { loading, error } = useContext(FeatureFlagsContext);
+  const [theme, setTheme] = useThemeToggle('light');
+
+  const isNewHeader = useFeatureFlag(FLAGS.NEW_HEADER);
+  const isDarkMode = useFeatureFlag(FLAGS.DARK_MODE);
+  const isBetaFeatures = useFeatureFlag(FLAGS.BETA_FEATURES);
+  const isNewPricing = useFeatureFlag(FLAGS.NEW_PRICING);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  }, [isDarkMode, setTheme]);
+
+  if (loading)
+    return (
+      <div className='wrapper'>
+        <div>Loading</div>
+      </div>
+    );
+  if (error)
+    return (
+      <div className='wrapper'>
+        <div>{error}</div>
+      </div>
+    );
+
+  return (
+    <div
+      className='flags-wrapper'
+      data-theme={theme}
+    >
+      <button>toggle</button>
+
+      {isNewHeader ? <NewHeader /> : <header>Old header</header>}
+
+      <main>
+        {isBetaFeatures ? <div>beta features</div> : <div>old content</div>}
+
+        {(isNewPricing && <div>New price list</div>) || (
+          <div>old price list</div>
+        )}
+      </main>
+
+      <div>{isDarkMode ? 'dark mode on' : 'light mode on'}</div>
+      <div>some random content here</div>
+    </div>
   );
 };
 
