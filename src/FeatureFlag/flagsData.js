20b8cc8ca7eff81082acf12f12045d59c95c36cc
NazSvn <idler90@gmail.com>
2024-11-08 23:44:04 +0800

    refactor: Export feature flags and add default values
    - Exported the FLAGS object to make it more accessible
    - Added a FLAG_CONFIG object that includes default values and descriptions for each flag

--- src/FeatureFlag/flagsData.js
+++ src/FeatureFlag/flagsData.js
@@ -1,8 +1,31 @@
-const FLAGS = {
+export const FLAGS = {
   NEW_HEADER: 'NEW_HEADER',
   DARK_MODE: 'DARK_MODE',
   BETA_FEATURES: 'BETA_FEATURES',
   NEW_PRICING: 'NEW_PRICING',
+};
+
+export const FLAG_CONFIG = {
+  [FLAGS.NEW_HEADER]: {
+    name: FLAGS.NEW_HEADER,
+    defaultValue: false,
+    description: 'Enables the new header design',
+  },
+  [FLAGS.DARK_MODE]: {
+    name: FLAGS.DARK_MODE,
+    defaultValue: false,
+    description: 'Enables dark mode theme',
+  },
+  [FLAGS.BETA_FEATURES]: {
+    name: FLAGS.BETA_FEATURES,
+    defaultValue: false,
+    description: 'Enables beta features',
+  },
+  [FLAGS.NEW_PRICING]: {
+    name: FLAGS.NEW_PRICING,
+    defaultValue: false,
+    description: 'Enables new pricing display',
+  },
 };
 
 const FeatureFlagService = () => {
